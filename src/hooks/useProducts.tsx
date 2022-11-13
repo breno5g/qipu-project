7b9bd06eee34b5c5b968493102ea70bef2897324
Breno Santos <brenosantos145@gmail.com>
2022-11-13 16:37:06 -0300

    feat: create function to add categories

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -4,10 +4,12 @@ import { EditedProductData, IProduct } from '~/interfaces/product';
 
 interface ProductsContextData {
   products: IProduct[];
+  categories: string[];
   selectedProduct: IProduct | null;
   deleteProduct(id: number): void;
   editProduct(data: EditedProductData): void;
   addProduct(data: IProduct): void;
+  addCategorie(data: string): void;
   selectProduct(data: IProduct | null): void;
 }
 
@@ -19,6 +21,7 @@ const ProductsContenxt = createContext<ProductsContextData>({} as ProductsContex
 
 export function ProductsProvider({ children }: ProductsProviderProps) {
   const [products, setProducts] = useState<IProduct[] | []>([]);
+  const [categories, setCategories] = useState<string[]>([]);
   const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
 
   const deleteProduct = (id: number) => {
@@ -45,8 +48,14 @@ export function ProductsProvider({ children }: ProductsProviderProps) {
     setSelectedProduct(data);
   };
 
+  const addCategorie = (data: string) => {
+    const handler = [...categories, data];
+    setCategories(handler);
+    localStorage.setItem('meiStorageCategories', JSON.stringify(handler));
+  };
+
   const saveToLocalStorage = (param?: string) => {
-    localStorage.setItem('meiStorage', JSON.stringify(param ? [] : products));
+    localStorage.setItem('meiStorageProducts', JSON.stringify(param ? [] : products));
   };
 
   useEffect(() => {
@@ -56,16 +65,29 @@ export function ProductsProvider({ children }: ProductsProviderProps) {
   }, [products]);
 
   useEffect(() => {
-    const storageProducts = localStorage.getItem('meiStorage');
+    const storageProducts = localStorage.getItem('meiStorageProducts');
+    const storageCategories = localStorage.getItem('meiStorageCategories');
 
     if (storageProducts?.length) {
       setProducts(JSON.parse(storageProducts));
+    }
+    if (storageCategories?.length) {
+      setCategories(JSON.parse(storageCategories));
     }
   }, []);
 
   return (
     <ProductsContenxt.Provider
-      value={{ products, selectedProduct, deleteProduct, editProduct, addProduct, selectProduct }}
+      value={{
+        products,
+        categories,
+        selectedProduct,
+        deleteProduct,
+        editProduct,
+        addProduct,
+        addCategorie,
+        selectProduct,
+      }}
     >
       {children}
     </ProductsContenxt.Provider>
