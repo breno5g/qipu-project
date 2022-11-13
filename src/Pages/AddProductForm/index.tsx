7ee2fc3c3b6d0179b43f736f91132aa59ee79532
Breno Santos <brenosantos145@gmail.com>
2022-11-13 15:01:03 -0300

    feat: create basic structure of new product form

--- src/Pages/AddProductForm/index.tsx
+++ src/Pages/AddProductForm/index.tsx
@@ -1,9 +1,134 @@
-function index() {
+import React, { useState } from 'react';
+import Select from 'react-select';
+import { Container } from './style';
+
+function Index() {
+  const [step, setStep] = useState(0);
+
+  const increaseStep = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (step != 5) {
+      setStep((prevstate) => prevstate + 1);
+    }
+  };
+
   return (
-    <div>
-      <h1>hoi</h1>
-    </div>
+    <Container step={step}>
+      <form>
+        <div className='carousel'>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯⬤⬤⬤⬤⬤</span>
+              <h1>Defina o nome do seu produto</h1>
+              <p>Escolha um nome para você e seu cliente poderem identificá-lo.</p>
+
+              <input type='text' />
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯◯⬤⬤⬤⬤</span>
+              <h1>Escreva quantos produtos você tem no estoque</h1>
+              <p>A quantidade ficará armazaenada para você controlar o seu estoque.</p>
+
+              <input type='number' />
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯◯◯⬤⬤⬤</span>
+              <h1>Descreva seu produto</h1>
+              <p>Coloque detalhes sobre o que está sendo oferecido.</p>
+
+              <input type='text' />
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯◯◯◯⬤⬤</span>
+              <h1>Adicione as categorias</h1>
+              <p>
+                Separar os produtos em categorias pode facilitar a sua organização e ajuda os
+                clientes encontrarem o que querem.
+              </p>
+
+              <Select
+                isMulti
+                name='categories'
+                closeMenuOnSelect={false}
+                placeholder={'Selecione as Categorias'}
+                theme={(theme) => ({
+                  ...theme,
+                  colors: {
+                    ...theme.colors,
+                    primary25: '#00d4ff',
+                  },
+                })}
+                styles={{
+                  option: (provided) => ({
+                    ...provided,
+                    color: 'black',
+                  }),
+                }}
+                options={[
+                  {
+                    label: 'bebida',
+                    value: 'bebida',
+                  },
+                  {
+                    label: 'café',
+                    value: 'café',
+                  },
+                ]}
+              />
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯◯◯◯◯⬤</span>
+              <h1>Adicione uma foto do seu produto</h1>
+              <p>
+                as fotos dos produtos aparecem para seus clientes em sua loja. É uma boa forma de
+                mostrar o que você está oferecendo.
+              </p>
+
+              <label htmlFor='image'>
+                <input
+                  type='file'
+                  id='image'
+                  accept='image/png, image/jpg, image/gif, image/jpeg'
+                />
+              </label>
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+          <fieldset>
+            <div className='top-content'>
+              <span>◯◯◯◯◯◯</span>
+              <h1>Defina o valor de venda</h1>
+              <p>
+                Para escoler o valor que será cobrado é importante você saber o custo do seu produto
+                e o lucro que você pretende obter.
+              </p>
+
+              <input className='currency' type='number' placeholder='R$ 0,00' />
+            </div>
+
+            <button onClick={increaseStep}>continuar</button>
+          </fieldset>
+        </div>
+      </form>
+    </Container>
   );
 }
 
-export default index;
+export default Index;
---
+++ src/Pages/AddProductForm/style.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+
+interface ContainerProps {
+  step: number;
+}
+
+export const Container = styled.main<ContainerProps>`
+  height: 100vh;
+  background-color: tomato;
+
+  form {
+    height: 100vh;
+    max-width: 550px;
+    background-color: teal;
+    overflow: hidden;
+
+    .carousel {
+      width: calc(100vw * 6);
+      max-width: calc(550px * 6);
+      display: grid;
+      grid-template-columns: repeat(6, 1fr);
+      transform: translateX(calc(${(props) => props.step} * -100vw));
+
+      fieldset {
+        height: 100vh;
+        min-height: 500px;
+        min-width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        border: none;
+        color: #fafafa;
+        background: linear-gradient(180deg, rgba(36, 127, 255, 1) 0%, rgba(0, 212, 255, 1) 100%);
+
+        div.top-content {
+          padding: 2rem;
+          display: flex;
+          flex-direction: column;
+          gap: 1.5rem;
+
+          span {
+            margin: 0 auto;
+            letter-spacing: 0.1rem;
+            font-size: 10px;
+            color: #fafafa99;
+          }
+
+          h1 {
+            text-align: center;
+          }
+
+          p {
+            white-space: pre-wrap;
+          }
+
+          input {
+            background-color: transparent;
+            border: none;
+            border-bottom: 2px solid #fafafa;
+            outline: none;
+            caret-color: #fafafa;
+            padding: 0.5rem;
+            color: white;
+            font-size: 1.5rem;
+          }
+
+          label {
+            cursor: pointer;
+            width: 300px;
+            height: 300px;
+            background-color: #33333380;
+            margin: 0 auto;
+            padding: 3rem 4rem;
+            position: relative;
+
+            &::before {
+              content: 'As dimensões da foto devem ser de 600 x 600';
+              display: inline-block;
+              text-align: center;
+            }
+
+            &::after {
+              content: '+';
+              position: absolute;
+              width: fit-content;
+              height: fit-content;
+              font-size: 4rem;
+              font-weight: 700;
+              margin: auto;
+              inset: 0;
+            }
+
+            input[type='file'] {
+              display: none;
+            }
+
+            .currency {
+              padding: 0.5rem 0.5rem 0.5rem 2.5rem;
+              position: relative;
+              display: inline-block;
+            }
+          }
+        }
+
+        button {
+          padding: 1rem;
+          font-size: 2rem;
+          font-weight: 500;
+          text-transform: uppercase;
+          background-color: transparent;
+          border: none;
+          color: inherit;
+          border-top: 2px solid #fafafa;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
